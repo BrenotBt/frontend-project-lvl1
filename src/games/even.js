f8ba4435edb9324196abe80e7dcbdf868775ec76
brenorbt <brenorbt@yandex.ru>
2019-12-12 15:51:47 +0400

    level5 with pairs modif

--- src/games/even.js
+++ src/games/even.js
@@ -4,34 +4,20 @@
  * Created by Tolstenko Alexander on 05.12.2019.
  */
 
-import hello from '..';
-import readlineSync from 'readline-sync';
+import exec from '..';
 import random from '../random';
 import iseven from '../iseven';
+import { cons, car, cdr } from '@hexlet/pairs';
 
 const task = 'Answer "yes" if the number is even, otherwise answer "no".';
+const maxInt = 1000;
 
-const ask = (attempt, Name) => {
-    const val = random(1000);
-    console.log("Question: "+val);
-    const answer = readlineSync.question("Your answer: ");
-    const res = iseven(val);
-    if((answer === 'yes' && res)  || (answer === 'no' && !res)){
-        console.log("Correct!");
-        attempt++;
-        if(attempt === 3)
-            console.log("Congratulations, " + Name + "!")
-        else
-            ask(attempt, Name);
-    }else{
-        console.log("'" + answer + "' is wrong answer ;(. Correct answer was '" + (res ? 'yes': 'no') + "'.");
-        console.log("Let's try again, " + Name + "!");
-        ask(0, Name);
-    }
+const getGameData = () => {
+    const question = random(maxInt);
+    if(iseven(question))
+        return cons(question, 'yes');
+    else
+        return cons(question, 'no');
 }
 
-export default () =>
-{
-    const Name = hello(task);
-    ask(0, Name);
-}
+export default () => exec(task, getGameData);
--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@
  */
 
 import readlineSync from 'readline-sync';
+import { car, cdr } from '@hexlet/pairs';
 
-export default (task) =>
+const roundsCount = 3;
+const firstRound = 1;
+
+export default (task, generateGameData) =>
 {
     console.log('Welcome to the Brain Games!')
     if(task !== undefined)
@@ -14,7 +18,32 @@ export default (task) =>
     console.log();
 
     const askName = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + askName + '!');
+    console.log(`Hello, ${askName}!`);
     console.log();
-    return askName;
+
+    const step = (ind) =>
+    {
+        if (ind > roundsCount) {
+            console.log(`Congratulations, ${askName}!`);
+            return;
+        }
+
+        const gameData = generateGameData();
+        const question = car(gameData);
+        const answer = cdr(gameData);
+
+        console.log(`Question: ${question}`);
+
+        const userAnswer = readlineSync.question('Your answer: ');
+        if (answer !== userAnswer) {
+            console.log(`${askName} is wrong answer ;(. Correct answer was '${answer}'.`);
+            console.log(`Let's try again, ${askName}`);
+            return step(firstRound);
+        }
+
+        console.log('Correct!');
+        step(ind + 1);
+    }
+
+    return step(firstRound);
 }
